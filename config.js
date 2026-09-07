64bccc886490f05438052ccf61ceb15af2c319db
ninoudy <ninoudy@gmail.com>
2026-09-07 11:53:31 +0900

    fix: update GAS web app endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.RESERVATION_CONFIG = {
-  GAS_WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbzstiH5RaC_MIpKZs8wIR9bPIH8Ictu5AOAgBp0ATU_lGBDAi4bhunr5v-wJ53_mabWdw/exec',
+  GAS_WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbzXqhU0w26M_J29XDTibT0Ob9OgymMnNUqPuH6t2Xo4vQ4RKgOxMhrzdirA_PQvewBwnw/exec',
   LIFF_ID: '2011264157-zCt4A73V',
 
   STORE_NAME: '蜂伏整体院',
